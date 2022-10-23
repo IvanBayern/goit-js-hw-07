d8fc1a5aae81c969e8a58a44d533376f378ed5b0
Ivan <96693815+IvanBayern@users.noreply.github.com>
2022-10-23 21:50:21 +0200

    add hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.append(...createGalleryItems(galleryItems));
+
+function createGalleryItems(items) {
+  return items.reduce((arr, { description, original, preview }) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.classList.add("gallery__item");
+    a.href = original;
+    img.classList.add("gallery__image");
+    img.src = preview;
+    img.alt = description;
+
+    a.append(img);
+    li.append(a);
+
+    arr = [...arr, li];
+    return arr;
+  }, []);
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
